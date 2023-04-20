592934bd6f8579a25ce005f686d4df1a540975ec
seedlessapple <luminousseedlessapple@gmail.com>
2023-04-20 18:12:19 +0100

    [FIX] fix import

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 import "styles/global.scss";
-import "styles/variables.scss";
 import "styles/fonts.scss";
 import Editor from "./editor/Editor";
 import Test from "test/Test";
