8df09b11fd9f52e9e96eaa4ec8e52e5a9d4f7da3
seedlessapple <luminousseedlessapple@gmail.com>
2023-04-20 18:02:38 +0100

    [FEAT] Add style Menu

---
+++ client/src/editor/layout/StyleMenu.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+
+type Props = {};
+
+const StyleMenu = (props: Props) => {
+  const [isShow, setIsShow] = useState(false);
+
+  useEffect(() => {
+    function handleSelectionChange(event: Event) {
+      const selection = document.getSelection();
+      if (selection && !selection?.isCollapsed) {
+        const range = selection.getRangeAt(0);
+        console.log(range.getBoundingClientRect());
+      }
+    }
+    console.log("a");
+
+    document.addEventListener("selectionchange", handleSelectionChange);
+    return () => {
+      document.removeEventListener("selectionchange", handleSelectionChange);
+    };
+  }, []);
+
+  return isShow ? <div>StyleMenu</div> : <></>;
+};
+
+export default StyleMenu;
