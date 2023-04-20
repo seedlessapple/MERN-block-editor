682ea6aa59a77e424e34130f207447585c121610
seedlessapple <luminousseedlessapple@gmail.com>
2023-04-20 18:00:14 +0100

    Create index.tsx

---
+++ client/src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+import "styles/global.scss";
+import "styles/variables.scss";
+import "styles/fonts.scss";
+import Editor from "./editor/Editor";
+import Test from "test/Test";
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<Editor />);
