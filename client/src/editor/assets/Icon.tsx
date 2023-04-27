c2f7e877f4545cf050f110289bdcc902a5019ce4
seedlessapple <luminousseedlessapple@gmail.com>
2023-04-27 10:39:02 +0100

    [FEAT] add classname props to Icon

--- client/src/editor/assets/Icon.tsx
+++ client/src/editor/assets/Icon.tsx
@@ -10,12 +10,14 @@ type Props = {
   rotate?: number;
   onClick?: React.MouseEventHandler<SVGSVGElement>;
   cursor?: CSSProperties["cursor"];
+  className?: string;
 };
 
 const Icon = (props: Props) => {
   return (
     <svg
       onClick={props.onClick && props.onClick}
+      className={props.className}
       shapeRendering={"auto"}
       fill={props.fill}
       transform={props.rotate ? `rotate(${props.rotate})` : undefined}
@@ -26,7 +28,7 @@ const Icon = (props: Props) => {
       viewBox="0 0 24 24"
       cursor={props.cursor && props.cursor}
     >
-    {iconsData[props.type]}
+      {iconsData[props.type]}
     </svg>
   );
 };
